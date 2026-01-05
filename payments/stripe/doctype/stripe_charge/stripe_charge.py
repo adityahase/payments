63f47b50a0a9ea3a7452577b4efe6ccc0373ed72
Aditya Hase <aditya@adityahase.com>
2026-01-05 10:21:55 +0530

    fix: Ignore null Charge.outcome

--- payments/stripe/doctype/stripe_charge/stripe_charge.py
+++ payments/stripe/doctype/stripe_charge/stripe_charge.py
@@ -19,26 +19,32 @@ class StripeCharge(Document, StripeHelper):
 			# But, there are no way to bulk fetch PaymentMethods
 			StripeCard.fill_everything(customer=charge.customer)
 
+		doc_dict = {
+			"doctype": "Stripe Charge",
+			"name": charge.id,
+			"status": unscrub(charge.status),
+			"payment_intent": charge.payment_intent,
+			"card": charge.payment_method,
+			"refunded": charge.refunded,
+			"dispute": charge.dispute,
+			"failure_code": unscrub(charge.failure_code),
+			"failure_message": charge.failure_message,
+			"payload": cls.serialize(charge),
+		}
+		if charge.outcome:
+			doc_dict.update(
+				{
+					"advice_code": unscrub(charge.outcome.advice_code),
+					"network_status": unscrub(charge.outcome.network_status),
+					"network_advice_code": charge.outcome.network_advice_code,
+					"network_decline_code": charge.outcome.network_decline_code,
+					"type": unscrub(charge.outcome.type),
+					"risk_level": unscrub(charge.outcome.risk_level),
+					"reason": unscrub(charge.outcome.reason),
+					"seller_message": charge.outcome.seller_message,
+				}
+			)
 		doc = frappe.get_doc(
-			{
-				"doctype": "Stripe Charge",
-				"name": charge.id,
-				"status": unscrub(charge.status),
-				"payment_intent": charge.payment_intent,
-				"card": charge.payment_method,
-				"refunded": charge.refunded,
-				"dispute": charge.dispute,
-				"failure_code": unscrub(charge.failure_code),
-				"failure_message": charge.failure_message,
-				"advice_code": unscrub(charge.outcome.advice_code),
-				"network_status": unscrub(charge.outcome.network_status),
-				"network_advice_code": charge.outcome.network_advice_code,
-				"network_decline_code": charge.outcome.network_decline_code,
-				"type": unscrub(charge.outcome.type),
-				"risk_level": unscrub(charge.outcome.risk_level),
-				"reason": unscrub(charge.outcome.reason),
-				"seller_message": charge.outcome.seller_message,
-				"payload": cls.serialize(charge),
-			}
+			doc_dict,
 		).insert(ignore_links=True)  # We cannot fetch all linked PaymentIntent without breaking backfill
 		doc.update_creation(charge.created)
